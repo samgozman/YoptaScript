737a9328a97dde2727a276980506ebdbbd0c50c8
Sam Gozman <eragonovich@gmail.com>
2016-12-04 01:06:37 +0300

    Добавлен двухсторонний перевод

--- demo/converter/converter.js
+++ demo/converter/converter.js
@@ -1,18 +1,24 @@
-var jsOnChange = document.getElementById('jstoyopta').addEventListener('keyup', converter, false);
-String.prototype.replaceAll = function (search, replacement) {
-    var target = this;
-    var re = new RegExp(search, 'g');
-    return this.replace(re, replacement);
-};
+document.getElementById('jstoyopta').addEventListener('keyup', function(){converter(true);}, false);
+document.getElementById('yopta').addEventListener('keyup', function(){converter(false);}, false);
 
-function converter() {
-    var jstoyopta = document.getElementById('jstoyopta').value;
-    // str.match(regexp)
-    var i = 0;
-    var yoptaNew = jstoyopta;
-    //Весь сок тут
-    for (i = 0; i < dictionary.length; i++) {
-        yoptaNew = yoptaNew.replaceAll('' + dictionary[i][0] + '', dictionary[i][1]);
+function converter(lang) {
+    if(lang) {
+        //переводим в йопту
+        var jstoyopta = document.getElementById('jstoyopta').value,
+            i = 0,
+            yoptaNew = jstoyopta;
+        for (i = 0; i < dictionary.length; i++) {
+            yoptaNew = yoptaNew.replaceAll('' + dictionary[i][0] + '', dictionary[i][1]);
+        }
+        document.getElementById('yopta').value = yoptaNew;
+    } else {
+        //переводим из йопты
+        var ystojs = document.getElementById('yopta').value,
+            i = 0, 
+            jsNew = ystojs;
+        for (i = 0; i < dictionary.length; i++) {
+            jsNew = jsNew.replaceAll('' + dictionary[i][1] + '', dictionary[i][0]);
+        }
+        document.getElementById('jstoyopta').value = jsNew;
     }
-    document.getElementById('yopta').value = yoptaNew;
 }
