9edaf72fd9a15831c2ef2dfe3f8a6b8dd7ed0d1e
Semen Shestakov <shestakovsemen@gmail.com>
2016-12-09 23:53:51 +0200

    Add dist

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const
   browserify = require('./gulp/browserify'),
   gulp = require('gulp');
 
-gulp.task('build', browserify('./src/browser', './dist/yopta.js'));
+gulp.task('build', browserify('./src/browser', './dist/yopta.min.js'));
 gulp.task('watch', () => gulp.watch('./src', ['build']));
 
 gulp.task('default', ['build']);
