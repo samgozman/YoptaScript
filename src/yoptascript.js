351df6bb67e313e4ca61017ec9670b572b74ed72
Sam Gozman <eragonovich@gmail.com>
2016-12-09 15:31:44 +0300

    Обновление заголовочной информации о версии

--- src/yoptascript.js
+++ src/yoptascript.js
@@ -1,5 +1,5 @@
 /*!
- * YoptaScript v0.2.6 (https://yopta.space)
+ * YoptaScript v0.2.7 (https://yopta.space)
  * Copyright (c) 2016 Yopta.Space project and Contributors
  * Licensed under the MIT license
  */
